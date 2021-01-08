91702a15af91759c73f9435e18d10c686f9e76d0
Kibreab Gizatu <kibreabg@gmail.com>
2021-01-08 13:50:35 +0300

    Material components declarations updated

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,7 +6,13 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
-const material = [MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatToolbarModule]
+const material = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatToolbarModule
+];
 
 @NgModule({
   imports: [material],
