6d6e3873e0584fb73d8e8b4d78b18fc0bbd3bed3
Kibreab Gizatu <kibreabg@gmail.com>
2021-01-08 13:25:47 +0300

    Angular Material 9 Updated

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const material = [MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatToolbarModule]
+
+const material = [MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatToolbarModule]
 
 @NgModule({
   imports: [material],
